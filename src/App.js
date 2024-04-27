ca2f5cc62139e596fdb822c81161c2a3c8bf77b4
sujith133 <sujithcharles007@gmail.com>
2024-04-28 04:36:44 +0530

    release-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,83 @@
+import TextField from '@mui/material/TextField';
 import './App.css';
+import { Button } from '@mui/material';
+import {useState} from 'react';
 
 function App() {
-
+  const [rate,setRate]=useState('')
+  const [amount,setAmount]=useState('')
+  const [tenure,setTenure]=useState('')
+  const [payable,setPayable]=useState('')
+  const [callLogs,setCallLogs]=useState([['rate','amount','tenure','total Amount']])
+  const tenureChanged=(event)=>{
+    setTenure(event.target.value)
+  } 
+  const rateChanged=(event)=>{
+    setRate(event.target.value)
+  } 
+  const amountChanged=(event)=>{
+    setAmount(event.target.value)
+  } 
+  const calculateIntrest =()=>{
+    setPayable(parseInt(amount)+(rate*amount*tenure)/100)
+    setCallLogs([...callLogs,[rate,amount,tenure,payable]])
+    console.log(payable,callLogs)
+    setCallLogs([...callLogs,[rate,amount,tenure,parseInt(amount)+(rate*amount*tenure)/100]])
+  }
   return (
     <div className='bg-container'>
-      <h1>intialized</h1>
+      <h1>Simple Intrest Calculator</h1>
+      
+      <div className='rowdivContainer'>
+      <TextField
+          required
+          id="outlined-number"
+          label="Amount"
+          type='number'
+          sx={{width:'40%', margin:3}}
+          value={amount}
+          onChange={(event)=>amountChanged(event)}
+        />
+        <TextField
+          required
+          id="outlined-number"
+          type='number'
+          label="tenure in years"
+          sx={{width:'40%', margin:3}}
+          value={tenure}
+          onChange={(event)=>tenureChanged(event)}
+        />
+        <TextField
+          required
+          id="outlined-number"
+          type='number'
+          label="rate of interest"
+          sx={{width:'40%', margin:3}}
+          value={rate}
+          onChange={(event)=>rateChanged(event)}
+        />
+        <TextField
+          required
+          id="outlined-read-only-input"
+          label="amount to be paid"
+          type='number'
+          
+          sx={{width:'40%', margin:3}}
+          value={payable}
+          
+        />
+        <Button onClick={calculateIntrest} sx={{width:'40%', margin:3}} variant='contained'>Calculate</Button>
+      </div>
+      <ul className='rowdivContainer scroll'>
+        {callLogs.map(each=>(
+          <li className='rowContainer'>
+            <h5>{each[0]}</h5>
+            <h5>{each[1]}</h5>
+            <h5>{each[2]}</h5>
+            <h5>{each[3]}</h5>
+          </li>
+        ))}
+      </ul>
           </div>
   );
 }
